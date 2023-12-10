e84aedfa6b1c5e8250b1509b415804083543b3a3
SzauterAbel <abel.szauter@gmail.com>
2023-12-10 17:19:12 +0100

    Change project architecture
    Signed-off-by: SzauterAbel <abel.szauter@gmail.com>

--- src/main/controller/app.py
+++ src/main/controller/app.py
@@ -2,15 +2,18 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
     text = request.form['inputText']
     # For now, just return the same text received from the user
     return render_template('index.html', outputText=text)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
