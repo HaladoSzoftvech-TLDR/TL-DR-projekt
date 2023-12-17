0eb0e01bbe5f4f50c4b3b25fb9174327c41c738b
SzauterAbel <abel.szauter@gmail.com>
2023-12-17 15:44:19 +0100

    Add path to find imports
    Signed-off-by: SzauterAbel <abel.szauter@gmail.com>

--- src/test/logic/test_model.py
+++ src/test/logic/test_model.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from main.tools.privacy_policy_generator import generate_pp_for_test
 from main.logic.model import summarize
 import pytest
