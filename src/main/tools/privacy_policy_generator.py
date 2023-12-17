cd7bc0237c906b0b902c3544084f2392a2161698
SzauterAbel <abel.szauter@gmail.com>
2023-12-17 15:49:58 +0100

    Add path to pp_generator to find imports
    Signed-off-by: SzauterAbel <abel.szauter@gmail.com>

--- src/main/tools/privacy_policy_generator.py
+++ src/main/tools/privacy_policy_generator.py
@@ -1,4 +1,7 @@
-from src.main.tools.pp_samples import *
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from main.tools.pp_samples import *
 import csv
 import random
 
