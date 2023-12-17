c778cfe54f5c0259c83e2233560ea1252615934b
szerencsi.kristof <szerencsi.kristof@gmail.com>
2023-12-17 20:30:08 +0100

    Added basic try except logic.

--- src/main/controller/app.py
+++ src/main/controller/app.py
@@ -29,22 +29,26 @@ def index():
 @app.route('/summarize', methods=['POST'])
 def summarize():
     input_text = ''
-    if 'file' in request.files and request.files['file'].filename != '':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        if not filename.endswith('.pdf'):
-            return 'Please upload a PDF file only'
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        pdf = PdfReader(pdf_path)
-        input_text = " ".join(page.extract_text() for page in pdf.pages)
-        os.remove(pdf_path)
-    elif 'inputText' in request.form and request.form['inputText'].strip() != '':
-        input_text = request.form['inputText']
-    else:
-        return 'No file or text provided'
+    try:
+        if 'file' in request.files and request.files['file'].filename != '':
+            file = request.files['file']
+            filename = secure_filename(file.filename)
+            if not filename.endswith('.pdf'):
+                return 'Please upload a PDF file only'
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            pdf = PdfReader(pdf_path)
+            input_text = " ".join(page.extract_text() for page in pdf.pages)
+            os.remove(pdf_path)
+        elif 'inputText' in request.form and request.form['inputText'].strip() != '':
+            input_text = request.form['inputText']
+        else:
+            return 'No file or text provided'
+    except Exception as e:
+        return f'An error occurred while reading the PDF file: {str(e)}. Please upload a different file.'
     summarized_text = model_summarize(input_text)
     return render_template('index.html', outputText=summarized_text, inputText=input_text)
+
 
 
 @app.route('/send_feedback', methods=['POST'])
